c6220366e18f89655712be9a090a19ffafa8760d
Josh Zimmerman <72105948+josh610@users.noreply.github.com>
2023-08-22 17:06:20 -0400

    made dropdown component searchable

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useState } from 'react'
-import '../../styles/defaults.css'
+import Select from 'react-select'
+
 
 interface DropdownProps {
     options: {
@@ -17,19 +17,12 @@ interface DropdownProps {
  * @returns 
  */
 const Dropdown = ({ options, onChange }: DropdownProps) => {
-    const [selectedOption, setSelectedOption] = useState(options[0].key)
-
     return (
-      <select className="border-2"
-        value={selectedOption}
-        onChange={e => {
-          if (onChange) onChange(options[e.target.selectedIndex].value)
-          return setSelectedOption(options[e.target.selectedIndex].key)}}>
-        {options.map(o => {
-          return (
-              <option key={o.key} value={o.key}>{o.label}</option>
-        )})}
-      </select>
+      <Select className="border-2 w-fit"
+        options={options}
+        defaultValue={options[0]}
+        onChange={e => {if (onChange) onChange(e?.value)}}
+      />
     );
   };
 
